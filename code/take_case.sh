43ca23ff247e871963a7bd5927621851941b98a8
darkangelex <dark75132angel@163.com>
2022-05-07 11:03:39 +0800

    gc -> imac

--- code/take_case.sh
+++ code/take_case.sh
@@ -1,6 +1,6 @@
 if [ -d test_case ]; then rm -rf test_case; fi; mkdir test_case;
 if [ -d initproc  ]; then rm -rf initproc;  fi; mkdir initproc;
 cp -r testsuits-for-oskernel/riscv-syscalls-testing/user/build/riscv64/. test_case/
-cp -f user/target/riscv64gc-unknown-none-elf/release/initproc initproc/
-cp -f user/target/riscv64gc-unknown-none-elf/release/user_shell initproc/
-cp -f user/target/riscv64gc-unknown-none-elf/release/run_all_case initproc/
+cp -f user/target/riscv64imac-unknown-none-elf/release/initproc initproc/
+cp -f user/target/riscv64imac-unknown-none-elf/release/user_shell initproc/
+cp -f user/target/riscv64imac-unknown-none-elf/release/run_all_case initproc/
